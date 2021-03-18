8ee8b7775b7cf9439bc826a61f1f9ccde0535434
Emilio Faria <2293459+ecfaria@users.noreply.github.com>
2021-03-18 16:08:53 +0000

    getting only custom properties

--- index.js
+++ index.js
@@ -1,10 +1,60 @@
 /* eslint-disable */
-const fs = require('fs');
-
 const postcss = require('postcss');
+const fs = require('fs');
+const postcssParser = require('postcss-values-parser');
+const parse = postcssParser.parse;
 
 module.exports = (opts = {}) => {
   // Work with options here
+
+  const getCustomPropertiesFromRoot = (root, opts = {}) => {
+    const customPropertiesFromHtmlElement = {};
+    const customPropertiesFromRootPseudo = {};
+
+    root.nodes.slice().forEach((rule) => {
+      const customPropertiesObject = isHtmlRule(rule)
+        ? customPropertiesFromHtmlElement
+        : isRootRule(rule)
+        ? customPropertiesFromRootPseudo
+        : null;
+
+      if (customPropertiesObject) {
+        rule.nodes.slice().forEach((decl) => {
+          if (isCustomDecl(decl)) {
+            const { prop } = decl;
+
+            // write the parsed value to the custom property
+            customPropertiesObject[prop] = parse(decl.value).nodes;
+          }
+        });
+      }
+    });
+
+    return {
+      ...customPropertiesFromRootPseudo,
+      ...customPropertiesFromHtmlElement,
+    };
+  };
+
+  const htmlSelectorRegExp = /^html$/i;
+  const rootSelectorRegExp = /^:root$/i;
+  const customPropertyRegExp = /^--[A-z][\w-]*$/;
+
+  const isHtmlRule = (node) =>
+    node.type === 'rule' &&
+    node.selector.split(',').some((item) => htmlSelectorRegExp.test(item)) &&
+    Object(node.nodes).length;
+  const isRootRule = (node) =>
+    node.type === 'rule' &&
+    node.selector.split(',').some((item) => rootSelectorRegExp.test(item)) &&
+    Object(node.nodes).length;
+
+  // whether the node is an custom property
+  const isCustomDecl = (node) =>
+    node.type === 'decl' && customPropertyRegExp.test(node.prop);
+
+  // whether the node is a parent without children
+  const isEmptyParent = (node) => Object(node.nodes).length === 0;
 
   const readFile = (from) =>
     new Promise((resolve, reject) => {
@@ -20,7 +70,7 @@ module.exports = (opts = {}) => {
   const getCustomPropertiesFromCssFile = async (from) => {
     const css = await readFile(from);
     const root = postcss.parse(css, { from });
-    console.log(root.nodes[0]);
+    return root;
   };
 
   const customPropertiesPromise = async (file) => {
@@ -29,7 +79,9 @@ module.exports = (opts = {}) => {
   };
 
   const asyncTransform = async (root) => {
-    const foo = await customPropertiesPromise(opts.importFrom[0]);
+    const rootRules = await customPropertiesPromise(opts.importFrom[0]);
+    const customProperties = getCustomPropertiesFromRoot(rootRules);
+    console.log(customProperties);
   };
 
   return {
